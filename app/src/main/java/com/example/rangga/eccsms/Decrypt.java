d90056bb208aca19075259b5de91718dde8b0de0
Rangga Abimanyu <ranggag51@gmail.com>
2020-01-21 22:50:22 +0700

    decrypt function can be used

--- app/src/main/java/com/example/rangga/eccsms/Decrypt.java
+++ app/src/main/java/com/example/rangga/eccsms/Decrypt.java
@@ -16,11 +16,10 @@ import java.security.spec.EncodedKeySpec;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
 
-import static com.example.rangga.eccsms.setting.privateKey;
-
 public class Decrypt extends AppCompatActivity {
     String msg;
     PrivateKey privateKey;
+    byte[] privateKeyBytes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,40 +30,37 @@ public class Decrypt extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        EditText encrypted_msg = findViewById(R.id.message_to_decrypt);
-        EditText decrypted_msg = (EditText)findViewById(R.id.decrypted_message);
+
+        EditText decrypted_msg = findViewById(R.id.decrypted_message);
+
+
         decrypted_msg.setText("2");
-        msg = encrypted_msg.getText().toString();
+
         decrypted_msg.setText("1");
-        privateKey = setting.getPrivateKey();
+        //privateKey = setting.getPrivateKey();
         decrypted_msg.setText("0.5");
 
-        /*byte[] privateKeyBytes = Base64.decode(_privateKey.getBytes(), Base64.DEFAULT);
 
 
-        try {
-            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-            EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(privateKeyBytes);
-            privateKey = keyFactory.generatePrivate(privateKeySpec);
 
-        } catch (NoSuchAlgorithmException e) {
-            EditText decrypted_msg = (EditText)findViewById(R.id.decrypted_message);
-            decrypted_msg.setText("no such algorithm");
-        } catch (InvalidKeySpecException e) {
-            EditText decrypted_msg = (EditText)findViewById(R.id.decrypted_message);
-            decrypted_msg.setText("INVALID KEY");
-        }
-        */
-        Button decrypt_button = (Button) findViewById(R.id.decrypt);
+        Button decrypt_button = findViewById(R.id.decrypt);
 
         decrypt_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 try {
-                    EditText decrypted_msg = (EditText)findViewById(R.id.decrypted_message);
-                    decrypted_msg.setText("0");
-                    String plaintext = RSA.decrypt(msg,privateKey);
-                    decrypted_msg.setText(plaintext);
+                    EditText encrypted_msg = findViewById(R.id.message_to_decrypt);
+                    msg = encrypted_msg.getText().toString();
+                    EditText key = (EditText)findViewById(R.id.private_key);
+                    String private_key = key.getText().toString();
+                    privateKeyBytes = Base64.decode(private_key.getBytes(), Base64.DEFAULT);
+                    KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+                    EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(privateKeyBytes);
+                    privateKey = keyFactory.generatePrivate(privateKeySpec);
+                    EditText decrypted_msg = findViewById(R.id.decrypted_message);
+                    //decrypted_msg.setText("0");
+                   String plaintext = RSA.decrypt(msg,privateKey);
+                   decrypted_msg.setText(plaintext);
 
                 } catch (Exception e) {
                     e.printStackTrace();
